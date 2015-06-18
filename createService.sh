35659b7f8e706d1384516d7597412f57a29b1163
efermin <efermin@contactop2p.com>
2015-06-18 15:45:37 -0430

    Se crearon los archivos para la instalacion del servicio

---
+++ createService.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+# Create by: Enrique Fermin 18Jun2015
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+option=$1
+
+case $option in
+
+"asr" )
+	sudo cp marcador.sh_asr /usr/local/bin/marcador.sh
+;;
+"apl" )
+	sudo cp marcador.sh_apl /usr/local/bin/marcador.sh
+;;
+esac
+
+user=$USER
+group=`groups $USER | awk -F": " '{print $2}'`
+
+sudo cp marcador /etc/init.d/marcador
+
+sudo touch /etc/default/marcador
+sudo echo $user >> /etc/default/marcador
+sudo echo $group >> /etc/default/marcador
+
+
+sudo chown $user.$group /usr/local/bin/marcador.sh
+sudo chown $user.$group /etc/default/marcador
+sudo chown $user.$group /etc/init.d/marcador
